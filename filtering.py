4ab9c632d73717fc14f00b3ea7ad87737aff54a0
Filip Frey <freyfilip3@gmail.com>
2021-04-26 14:32:10 +0200

    filtering bug fixes

--- filtering.py
+++ filtering.py
@@ -7,10 +7,10 @@ def calculate_significance_dependency_matrix(import_result: Result):
         significance_dependency[event] = dict()
         for successor_name, successor_count in counter.most_common():
             try:
-                t21 = significance_dependency[successor_name][event]
+                t21 = import_result.direct_succession[successor_name][event]
             except KeyError:
                 t21 = 0
-            t12 = significance_dependency[event][successor_count]
+            t12 = import_result.direct_succession[event][successor_name]
             significance_dependency[event][successor_name] = (t12 - t21)/ (t12 + t21 +1)
 
     return significance_dependency
@@ -19,14 +19,17 @@ def calculate_2loop_matrix(import_result: Result):
     two_loop = dict()
     for event, counter in import_result.direct_succession.items():
         for successor_name, successor_count in counter.most_common():
-            counter = 0
-            for trace in import_result.traces_df.iterrows():
-                counter += trace.count(';'.join([event, successor_name, event])[:-1])
-            if counter != 0:
-                if two_loop[event]:
-                    two_loop[event][successor_name] = counter
-                else:
-                    two_loop[event] = dict([(successor_name, counter)])
+            if successor_name != event:
+                counter_now = 0
+                substring = ';'.join([event, successor_name, event])
+                for trace in import_result.traces_df.iterrows():
+                    activity_str = trace[1]['Activity']
+                    counter_now += activity_str[:-1].count(substring)
+                if counter_now != 0:
+                    if event in two_loop:
+                        two_loop[event][successor_name] = counter_now
+                    else:
+                        two_loop[event] = dict([(successor_name, counter_now)])
 
     two_loop_significance = dict()
     for outside, outside_dict in two_loop.items():
@@ -42,8 +45,8 @@ def calculate_2loop_matrix(import_result: Result):
                 if inside not in two_loop_significance[outside] and outside not in two_loop_significance[inside]:
                     two_loop_significance[outside][inside] = value
             except KeyError:
-                if two_loop_significance[outside]:
-                    two_loop_significance[outside] = value
+                if outside in two_loop_significance:
+                    two_loop_significance[outside][inside] = value
                 else:
                     two_loop_significance[outside] = dict([(inside, value)])
 
@@ -62,11 +65,12 @@ def filter_network_by_matrices(sd_dict: Dict[str, Dict[str, float]], two_loop_di
         for eventB, value in dictA.items():
             if eventB == eventA:
                 self_loop_events.append(eventB)
-            if value == 0:
+            # TODO
+            elif value == 0:
                 parallel_tuples.append((eventA, eventB))
 
             if threshold <= value:
-                if filtered_direct_succession[eventA]:
+                if eventA in filtered_direct_succession:
                     filtered_direct_succession[eventA][eventB] = value
                 else:
                     filtered_direct_succession[eventA] = dict([(eventB, value)])
@@ -74,7 +78,7 @@ def filter_network_by_matrices(sd_dict: Dict[str, Dict[str, float]], two_loop_di
     for out_loop , dict_out in two_loop_dict.items():
         for in_loop, value in dict_out.items():
             if value >= threshold:
-                if filtered_out_two_loop[out_loop]:
+                if out_loop in filtered_out_two_loop:
                     filtered_out_two_loop[out_loop][in_loop] = value
                 else:
                     filtered_out_two_loop[out_loop] = dict([(in_loop, value)])
